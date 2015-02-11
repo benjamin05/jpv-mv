3b97a152dce4f2c5dc5b9f9ccb9f60b6e4d7a7a1
benjamin <benjamin.martinez@lux.mx>
2015-02-11 08:23:38 -0600

    actualizacion en archivo ZV dev en efectivo

--- core/src/main/java/mx/lux/pos/model/DevTmp.java
+++ core/src/main/java/mx/lux/pos/model/DevTmp.java
@@ -20,6 +20,11 @@ public class DevTmp implements Serializable {
     String factura;
     Integer idPago;
     String idTerminal;
+    String nombre;
+    String idBanco;
+    String cuenta;
+    String clabe;
+    String correo;
 
     public DevTmp( Integer idPago ){
       this.idPago = idPago;
@@ -28,12 +33,26 @@ public class DevTmp implements Serializable {
     }
 
     public void AcumulaDevoluciones( Devolucion devolucion ){
+      String[] dataDev = devolucion.getDevEfectivo().split(",");
       idFactura = devolucion.getModificacion().getIdFactura();
       idFormaPago = devolucion.getIdFormaPago();
       monto = monto.add(devolucion.getMonto());
       idMod = devolucion.getIdMod();
       factura = devolucion.getModificacion().getNotaVenta().getFactura();
       idTerminal = devolucion.getPago().getIdTerminal();
+      if(dataDev.length > 1){
+        nombre = dataDev[0];
+        idBanco = dataDev[1];
+        cuenta = dataDev[2];
+        clabe = dataDev[3];
+        correo = dataDev[4];
+      } else {
+        nombre = "";
+        idBanco = "";
+        cuenta = "";
+        clabe = "";
+        correo = "";
+      }
     }
 
 
@@ -91,5 +110,45 @@ public class DevTmp implements Serializable {
 
     public void setIdTerminal(String idTerminal) {
         this.idTerminal = idTerminal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdBanco() {
+        return idBanco;
+    }
+
+    public void setIdBanco(String idBanco) {
+        this.idBanco = idBanco;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getClabe() {
+        return clabe;
+    }
+
+    public void setClabe(String clabe) {
+        this.clabe = clabe;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 }
--- empaqueta.sh
+++ empaqueta.sh
@@ -1,5 +1,5 @@
 CIERRE_HOME=/home/paso/cierre
 cd $CIERRE_HOME
-tar -cvf /home/paso/por_enviar/120172211.MAEM *22-11-2014.* *.inv
-tar -cvf /home/paso/ventas/120172211.MAEM *22-11-2014.* *.inv
-tar -cvf /home/drop/Dropbox/MAE/120172211.MAEM *22-11-2014.* *.inv
+tar -cvf /home/paso/por_enviar/120321002.MAEM *10-02-2015.* *.inv
+tar -cvf /home/paso/ventas/120321002.MAEM *10-02-2015.* *.inv
+tar -cvf /home/drop/Dropbox/MAE/120321002.MAEM *10-02-2015.* *.inv
