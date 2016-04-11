800c7c794180e78b56935e63750049c45bba87bc
benjamin <benjamin.martinez@lux.mx>
2016-04-11 13:03:42 -0500

    database

--- empaqueta.sh
+++ empaqueta.sh
@@ -1,5 +1,5 @@
 CIERRE_HOME=/home/paso/cierre
 cd $CIERRE_HOME
-tar -cvf /home/paso/por_enviar/120490704.MAEM *07-04-2016.* *.inv
-tar -cvf /home/paso/ventas/120490704.MAEM *07-04-2016.* *.inv
-tar -cvf /home/drop/Dropbox/MAE/120490704.MAEM *07-04-2016.* *.inv
+tar -cvf /home/paso/por_enviar/120140804.MAEM *08-04-2016.* *.inv
+tar -cvf /home/paso/ventas/120140804.MAEM *08-04-2016.* *.inv
+tar -cvf /home/drop/Dropbox/MAE/120140804.MAEM *08-04-2016.* *.inv
