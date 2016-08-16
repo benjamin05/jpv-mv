416bb6ea73711dcf55c0cfbbad6d349cf44d6729
benjamin <benjamin.martinez@lux.mx>
2016-08-16 13:06:41 -0500

    databse

--- empaqueta.sh
+++ empaqueta.sh
@@ -1,6 +1,6 @@
 CIERRE_HOME=/home/paso/cierre
 cd $CIERRE_HOME
-tar -cvf /home/paso/por_enviar/128021108.MAEM *11-08-2016.* *.inv
-tar -cvf C:/Users/mensajero/ventas/128021108.MAEM *11-08-2016.* *.inv
-tar -cvf C:/Dropbox/12802/128021108.MAEM *11-08-2016.* *.inv
+tar -cvf /home/paso/por_enviar/129680908.MAEM *09-08-2016.* *.inv
+tar -cvf C:/Users/mensajero/ventas/129680908.MAEM *09-08-2016.* *.inv
+tar -cvf C:/Dropbox/12968/129680908.MAEM *09-08-2016.* *.inv
 
